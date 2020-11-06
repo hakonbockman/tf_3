8c0337ef12f7c3331d24292365dc4987c3ef2dfa
Håkon Sørensen Bøckman <hakonbockman@gmail.com>
2020-11-06 13:00:11 +0100

    yas

--- main2.py
+++ main2.py
@@ -5,8 +5,11 @@ import PIL.Image
 import tensorflow as tf
 import pathlib
 import matplotlib.pyplot as plt
+import pandas as pd
 
+from tensorflow import keras
 
+''' LOAD THE DATA AND SPLIT '''
 data_dir_IR = pathlib.Path('./Combined/IR/')
 data_dir_Visual = pathlib.Path('./Combined/Visual/')
 
@@ -15,6 +18,7 @@ batch_size = 32
 img_height = 300
 img_width = 300
 
+''' TRAINING DATA'''
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir_IR,
   validation_split=0.2,
@@ -23,7 +27,7 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-
+''' VALIDATION DATA'''
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir_IR,
   validation_split=0.1,
@@ -45,26 +49,17 @@ for images, labels in train_ds.take(1):
     plt.axis("off")
 plt.show()
 
-''' Alternative method '''
-import tensorflow_datasets as tfds
-
-
-tensorflow_datasets.disable_progress_bar()
-
-train_ds, validation_ds, test_ds = tensorflow_datasets.load(
-
-)
-
-
-
-
-
-
-
-
-
-
-
+#''' Alternative method '''
+#'''
+#import tensorflow_datasets as tfds
+#
+#
+#tensorflow_datasets.disable_progress_bar()
+#
+#train_ds, validation_ds, test_ds = tensorflow_datasets.load(
+#  
+#)
+#'''
 
 
 
@@ -88,8 +83,36 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 train_ds = train_ds.cache().prefetch(buffer_size = AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size = AUTOTUNE)
 
-''' Train the model'''
+
+
+''' Compile the model'''
+
+batch_size = 32
+img_height = 300
+img_width = 300
 
 num_classes = train_ds.class_names
 
-model = tf.keras.Se
+# Load in the Xception network pretrained on the imagenet dataset.
+base_model = keras.applications.Xception(
+  weights='imagenet',
+  input_shape=(img_width, img_height, 3),
+  include_top=False
+)
+
+base_model = False # Freeze the model, so we dont dissrupt the allready trained weights.
+
+inputs = keras.Input(shape=(img_width, img_height, 3))
+
+x = base_model(inputs, training = False)
+
+outputs = keras.layers.Dense(1)(x)
+
+model = keras.Model(inputs, outputs)
+
+model.compile(
+  optimizer = keras.optimizers.Adam(),
+  loss = keras.losses.BinaryCrossentropy(from_logits=True),
+  metrics = [keras.memtrics.BinaryAccuracy()]
+)
+
